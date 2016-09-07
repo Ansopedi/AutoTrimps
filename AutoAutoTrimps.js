9116d902d866b4942339612c0b916a05b93c0a0f
Ansopedi <Ansopedi@web.de>
2016-09-07 21:53:00 +0200

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1731,7 +1731,7 @@ function autoStance() {
         var Sstance = 0.125;
         var ovkldmg = avgDamage * Sstance * (game.portal.Overkill.level*0.005);
         //are we going to overkill in S?
-        ovklHDratio = ovkldmg/(getEnemyMaxHealth(game.global.world)*getCorruptScale("health")*7);
+        ovklHDratio = ovkldmg/(getEnemyMaxHealth(game.global.world,1,true)*getCorruptScale("health")*7);
         hiderwindow = ovklHDratio*100;
         Area51i = ovkldmg;
         Area60i = getEnemyMaxHealth(game.global.world)*getCorruptScale("health")*7;
