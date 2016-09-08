a1878fc14fbcc346f4e1b069abfdd8d2cc584d21
Ansopedi <Ansopedi@web.de>
2016-09-08 11:01:01 +0200

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1740,7 +1740,15 @@ function autoStance() {
              //enoughDamage = true; enoughHealth = true; shouldFarm = false;
         }
     }
-    if (game.global.mapsActive && (getCurrentEnemy(1).name == "Jestimp" || getCurrentEnemy(1).name == "Chronoimp" ||  (hiderwindow > 0.02)) {
+    if (game.global.mapsActive && (getCurrentEnemy(1).name == "Jestimp" || getCurrentEnemy(1).name == "Chronoimp" ||  (hiderwindow > 0.02 && getCurrentMapObject().location != "Void"))) {
+ -    	setFormation(4);
+ -    	return;
+ -    }
+ if (game.global.mapsActive && (getCurrentEnemy(1).name == "Jestimp" || getCurrentEnemy(1).name == "Chronoimp" ||  (hiderwindow > 20 && getCurrentMapObject().location == "Void"))) {
+ -    	setFormation(4);
+ -    	return;
+ -    }
+    if (!game.global.mapsActive && (hiderwindow > 20)) {
     	setFormation(4);
     	return;
     }
