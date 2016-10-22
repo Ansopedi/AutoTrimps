c4da3a36515ec794651030427dc3b6be80fcf3f2
Ansopedi <Ansopedi@web.de>
2016-10-22 16:01:48 +0200

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -484,45 +484,9 @@ function initializeAutoTrimps() {
 }
 
 function workerRatios() {
-    if (game.resources.trimps.realMax() > 50000000) {
-        if (game.global.turkimpTimer > 0 && game.global.world == 9200 && getBuildingItemPrice(game.buildings.Warpstation, "gems", false, 1) > game.resources.gems.owned) { // Testing insted of: !200
-        autoTrimpSettings.FarmerRatio.value = '60';
-        autoTrimpSettings.LumberjackRatio.value = '5';
-        autoTrimpSettings.MinerRatio.value = '5';
-        }else if (game.buildings.Tribute.owned < 1100) {
-        autoTrimpSettings.FarmerRatio.value = '4';
-        autoTrimpSettings.LumberjackRatio.value = '1';
-        autoTrimpSettings.MinerRatio.value = '45';
-        } else if (game.buildings.Tribute.owned < 2000 && game.buildings.Tribute.owned >= 1100) {
-        autoTrimpSettings.FarmerRatio.value = '4';
-        autoTrimpSettings.LumberjackRatio.value = '1';
-        autoTrimpSettings.MinerRatio.value = '95';
-        } else if (game.global.world > 9200) {
-        autoTrimpSettings.FarmerRatio.value = '1';
-        autoTrimpSettings.LumberjackRatio.value = '5';
-        autoTrimpSettings.MinerRatio.value = '24';
-        } else if (game.global.world == 9200) {
-        autoTrimpSettings.FarmerRatio.value = '1';
-        autoTrimpSettings.LumberjackRatio.value = '11';
-        autoTrimpSettings.MinerRatio.value = '48';
-        } else  if (game.buildings.Tribute.owned < 3000) {
-        autoTrimpSettings.FarmerRatio.value = '1';
-        autoTrimpSettings.LumberjackRatio.value = '1';
-        autoTrimpSettings.MinerRatio.value = '98';
-        } else {
         autoTrimpSettings.FarmerRatio.value = '1';
         autoTrimpSettings.LumberjackRatio.value = '1';
         autoTrimpSettings.MinerRatio.value = '500';
-        }
-    } else if (game.resources.trimps.realMax() > 500000) {
-        autoTrimpSettings.FarmerRatio.value = '4';
-        autoTrimpSettings.LumberjackRatio.value = '4';
-        autoTrimpSettings.MinerRatio.value = '16';
-    } else {
-        autoTrimpSettings.FarmerRatio.value = '1';
-        autoTrimpSettings.LumberjackRatio.value = '1';
-        autoTrimpSettings.MinerRatio.value = '1';
-    }
 }
 
 //An error-resilient function that will actually purchase buildings and return a success status
@@ -1122,108 +1086,14 @@ function autoLevelEquipment() {
 }
 
 //"Auto Gather/Build"
-function manualLabor() {
-    var ManualGather = 'metal';
-    var breedingTrimps = game.resources.trimps.owned - game.resources.trimps.employed;    
-
-    //FRESH GAME NO HELIUM CODE.
-    if (game.global.world <=3 && game.global.totalHeliumEarned<=100) {
-        var breedingTrimps = game.resources.trimps.owned - game.resources.trimps.employed;
-        if (game.global.buildingsQueue.length == 0 && (game.global.playerGathering != 'trimps' || game.buildings.Trap.owned == 0)){
-            if (!game.triggers.wood.done || game.resources.food.owned < 10 || Math.floor(game.resources.food.owned) < Math.floor(game.resources.wood.owned))
-                setGather('food');
-            else
-                setGather('wood');
-        }
-    }
-    
-    if(breedingTrimps < 5 && game.buildings.Trap.owned == 0 && canAffordBuilding('Trap')) {
-        //safeBuyBuilding returns false if item is already in queue
-        if(!safeBuyBuilding('Trap'))
-            setGather('buildings');
-    }
-    else if (breedingTrimps < 5 && game.buildings.Trap.owned > 0) {
-        setGather('trimps');
-    }
-    else if (game.resources.science.owned < 100 && document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden') setGather('science');
-        //if we have more than 2 buildings in queue, or (our modifier is real fast and trapstorm is off), build                      
-    else if (!game.talents.foreman.purchased && (game.global.buildingsQueue.length ? (game.global.buildingsQueue.length > 1 || game.global.autoCraftModifier == 0 || (getPlayerModifier() > 1000 && game.global.buildingsQueue[0] != 'Trap.1')) : false)) {
-        // debug('Gathering buildings??');
-        setGather('buildings');
-    }
-        //if trapstorm is off (likely we havent gotten it yet, the game is still early, buildings take a while to build ), then Prioritize Storage buildings when they hit the front of the queue (should really be happening anyway since the queue should be >2(fits the clause above this), but in case they are the only object in the queue.)
-    else if (!game.global.trapBuildToggled && (game.global.buildingsQueue[0] == 'Shed.1' || game.global.buildingsQueue[0] == 'Barn.1' || game.global.buildingsQueue[0] == 'Forge.1')){
-        setGather('buildings');
-    }
-        //if we have some upgrades sitting around which we don't have enough science for, gather science
-    else if (game.resources.science.owned < scienceNeeded && document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden') {
-        // debug('Science needed ' + scienceNeeded);
+function manualLabor() {  
+    if (game.resources.science.owned < scienceNeeded && document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden') {
         if (getPlayerModifier() < getPerSecBeforeManual('Scientist') && game.global.turkimpTimer > 0){
-            //if manual is less than half of science production switch on turkimp
             setGather('metal');
         }
         else {
             setGather('science');
         }
-    }
-    else if (getPageSetting('TrapTrimps') && parseInt(getPageSetting('GeneticistTimer')) < getBreedTime(true)){
-        //combined to optimize code.
-        if (game.buildings.Trap.owned < 1 && canAffordBuilding('Trap')) { 
-            safeBuyBuilding('Trap');
-            setGather('buildings');
-        }
-        else if (game.buildings.Trap.owned > 0)
-            setGather('trimps');
-    }
-
-    else {
-        var manualResourceList = {
-            'food': 'Farmer',
-            'wood': 'Lumberjack',
-            'metal': 'Miner',
-        };
-        var lowestResource = 'food';
-        var lowestResourceRate = -1;
-        var haveWorkers = true;
-        for (var resource in manualResourceList) {
-            var job = manualResourceList[resource];
-            var currentRate = game.jobs[job].owned * game.jobs[job].modifier;
-            // debug('Current rate for ' + resource + ' is ' + currentRate + ' is hidden? ' + (document.getElementById(resource).style.visibility == 'hidden'));
-            if (document.getElementById(resource).style.visibility != 'hidden') {
-                // debug('INNERLOOP for resource ' +resource);
-                if (currentRate === 0) {
-                    currentRate = game.resources[resource].owned;
-                    // debug('Current rate for ' + resource + ' is ' + currentRate + ' lowest ' + lowestResource + lowestResourceRate);
-                    if ((haveWorkers) || (currentRate < lowestResourceRate)) {
-                        // debug('New Lowest1 ' + resource + ' is ' + currentRate + ' lowest ' + lowestResource + lowestResourceRate+ ' haveworkers ' +haveWorkers);
-                        haveWorkers = false;
-                        lowestResource = resource;
-                        lowestResourceRate = currentRate;
-                    }
-                }
-                if ((currentRate < lowestResourceRate || lowestResourceRate == -1) && haveWorkers) {
-                    // debug('New Lowest2 ' + resource + ' is ' + currentRate + ' lowest ' + lowestResource + lowestResourceRate);
-                    lowestResource = resource;
-                    lowestResourceRate = currentRate;
-                }
-            }
-            // debug('Current Stats ' + resource + ' is ' + currentRate + ' lowest ' + lowestResource + lowestResourceRate+ ' haveworkers ' +haveWorkers);
-        }
-
-        if (game.global.playerGathering != lowestResource && !haveWorkers && !breedFire) {
-            // debug('Set gather lowestResource');
-            setGather(lowestResource);
-        } else if (game.global.turkimpTimer > 0) {
-            //debug('Set gather ManualGather');
-            setGather('metal');
-        } else  if (game.resources.science.owned < getPsString('science', true) * 60 && document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden' && game.global.turkimpTimer < 1 && haveWorkers) {
-            setGather('science');
-        }
-        else if(getPageSetting('TrapTrimps') && game.global.trapBuildToggled == true && game.buildings.Trap.owned < 10000)
-            setGather('buildings');
-        else if (document.getElementById('scienceCollectBtn').style.display != 'none' && document.getElementById('science').style.visibility != 'hidden')
-            setGather('science');
-        
     }
 }
 
