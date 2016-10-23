8502a76242e53c0d54c7e979e1e99be493b1cb8c
Ansopedi <Ansopedi@web.de>
2016-10-23 16:02:12 +0200

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1140,8 +1140,8 @@ function autoStance() {
              //enoughDamage = true; enoughHealth = true; shouldFarm = false;
         }
     }
-    if (game.global.mapsActive && (getCurrentEnemy(1).name == "Jestimp" || getCurrentEnemy(1).name == "Chronoimp" ||  (hiderwindow > 0.75 && getCurrentMapObject().location != "Void"))){ setFormation(4); return;}
-    if (game.global.mapsActive && (getCurrentEnemy(1).name != "Jestimp" && getCurrentEnemy(1).name != "Chronoimp" &&  (hiderwindow < 0.75 && getCurrentMapObject().location != "Void"))){ setFormation(2); return;}
+    if (game.global.mapsActive && (getCurrentEnemy(1).name == "Jestimp" || getCurrentEnemy(1).name == "Chronoimp" ||  (hiderwindow > (960*0.12/mutations.Corruption.statScale(10)) && getCurrentMapObject().location != "Void"))){ setFormation(4); return;}
+    if (game.global.mapsActive && (getCurrentEnemy(1).name != "Jestimp" && getCurrentEnemy(1).name != "Chronoimp" &&  (hiderwindow < (960*0.12/mutations.Corruption.statScale(10)) && getCurrentMapObject().location != "Void"))){ setFormation(2); return;}
     if (game.global.mapsActive && (getCurrentEnemy(1).name == "Jestimp" || getCurrentEnemy(1).name == "Chronoimp" ||  (hiderwindow > 960 && getCurrentMapObject().location == "Void"))){ setFormation(4); return;}
     if (!game.global.mapsActive && (hiderwindow > 660)){ setFormation(4); return;}
     
