4a9abbc5e64ee1fb0a06e47aac4c975cc6db5cd3
Ansopedi <Ansopedi@web.de>
2016-10-28 15:45:42 +0200

    Update AutoAutoTrimps.js

--- AutoAutoTrimps.js
+++ AutoAutoTrimps.js
@@ -1376,7 +1376,7 @@ function autoMap() {
         }		
     }
 
-    if (getPageSetting('GetNurseriesEarly')) {
+    if (getPageSetting('GetNurseriesEarly') && game.global.world == 23 && game.global.mapBonus == 0) {
          shouldDoMaps = true;
      }
     var shouldDoSpireMaps = false;
